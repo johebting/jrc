145b4de196a8b284f9f5aa26054d36798708b128
Unknown <johebting@gmail.com>
2017-10-19 00:29:33 +0200

    Better formatting of table output

--- lib/jrc/tableizer.ex
+++ lib/jrc/tableizer.ex
@@ -4,18 +4,13 @@ defmodule Jrc.Tableizer do
 
   @moduledoc false
 
-  @title "Inputs since "
+  @title "JIRA Report"
   @header ["Date", "Issues", "Input", "Total"]
   @total_index 3
 
   def tableize(tree) do
-
     rows = trans(tree)
 
-    IO.inspect(rows)
-
-    # IO.inspect(rows)
-    # Table.new(rows, @header, @title)
     Table.new
     |> Table.put_title(@title)
     |> Table.put_header(@header)
@@ -29,7 +24,7 @@ defmodule Jrc.Tableizer do
     limit = Application.get_env(:jrc, :max_daily_input)
     total_header = Enum.fetch!(@header, @total_index)
 
-    if total_header != value && String.to_integer(value, 10) < limit do
+    if total_header != value && "" != String.trim(value) && String.to_integer(value, 10) < limit do
       [:red, text]
     else
       text
@@ -38,12 +33,21 @@ defmodule Jrc.Tableizer do
 
   def trans(tree) do
     tree
-    |> Enum.map(fn({date, inputs}) ->
-      issues = inputs |> Map.keys
-      times_spent = inputs |> Map.values
-      total = times_spent |> Enum.sum
+    |> Enum.reduce([], fn(branch, acc) ->
+      
+      date = elem(branch, 0)
+      inputs = elem(branch, 1)
+      total_time = inputs |> Map.values |> Enum.sum
 
-      [date, issues, times_spent, total]
+      [first_input | other_inputs] = inputs |> Map.to_list
+
+      acc
+      |> Kernel.++(
+        [[date, elem(first_input, 0), elem(first_input, 1), total_time]]
+      )
+      |> Kernel.++(
+        other_inputs |> Enum.map(&([nil, elem(&1, 0), elem(&1, 1), nil]))
+      )
     end)
   end
 end
